975c9c6a813cc76df6ce49b311e3e3a2d880ad91
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-04-14 22:39:14 +0200

    JS logic for Age calculation

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const calculateAge = () => {
     m3 = 12 + m2 - m1;
   }
   if (d2 >= d1) {
-    d3 - d2 - d1;
+    d3 = d2 - d1;
   } else {
     m3--;
     d3 = getDaysInMonth(y1, m1) + d2 - d1;
@@ -31,7 +31,7 @@ const calculateAge = () => {
     m3 = 11;
     y3--;
   }
-  result.innerHTML = `You are ${y3} years, ${m3} months and ${d3} days old`;
+  result.innerHTML = `Ooo My GOD!! You are ${y3} years, ${m3} months and ${d3} days old, That is an achevement !!!`;
 };
 const getDaysInMonth = (year, month) => {
   return new Date(year, month, 0).getDate();
